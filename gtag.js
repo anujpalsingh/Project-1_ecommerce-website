45d561693dfa953f27864546b18024ac178e96f8
Anuj Pal Singh <108212738+anujpalsingh@users.noreply.github.com>
2024-08-02 17:56:53 +0000

    initial

---
+++ gtag.js
@@ -0,0 +1,6 @@
+const GA_MEASUREMENT_ID = "G-DZDK7934TE"
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', GA_MEASUREMENT_ID);
